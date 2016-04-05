e366703b7290a81651986b62c39053d7853f33e6
RKrueger <student@rkrueger.local>
2016-04-05 12:19:04 -0500

    Initial Commit

---
+++ Road Kill FactsTests/Road_Kill_FactsTests.swift
@@ -0,0 +1,36 @@
+//
+//  Road_Kill_FactsTests.swift
+//  Road Kill FactsTests
+//
+//  Created by RKrueger on 4/5/16.
+//  Copyright © 2016 RKrueger. All rights reserved.
+//
+
+import XCTest
+@testable import Road_Kill_Facts
+
+class Road_Kill_FactsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
