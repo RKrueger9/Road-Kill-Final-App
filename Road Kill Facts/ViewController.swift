fd2ef0455385088513fae524a424a8fbed202a47
Student <student@bluc02mf24vfh51.local>
2016-04-06 11:58:10 -0500

    test 2

--- Road Kill Facts/ViewController.swift
+++ Road Kill Facts/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // test from tyler
     }
 
     override func didReceiveMemoryWarning()
