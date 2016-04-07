f33fb57d4d407c0b9084785d575b3ecbc5055861
RKrueger <student@rkrueger.local>
2016-04-07 12:14:44 -0500

    Added camera and library button as well as an Image view to the story board

--- Road Kill Facts/ViewController.swift
+++ Road Kill Facts/ViewController.swift
@@ -4,28 +4,28 @@
 //
 //  Created by RKrueger on 4/5/16.
 //  Copyright © 2016 RKrueger. All rights reserved.
-//  I'm deciding to add some more comments
-//  More comments
-//  Even more comments
+// 
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning()
+    @IBAction func onCameraButtonTap(sender: AnyObject)
     {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-        //testing an idea
-        //testing from Ryne
+        
     }
-
-
+    
+    @IBAction func onLibraryButtonTap(sender: AnyObject)
+    {
+        
+    }
+    
 }
 
