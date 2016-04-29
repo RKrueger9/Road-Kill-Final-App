7e0441e5e3d20cecea432fcb4dda14bf71721437
JonathanMarch <student@bluc02mg0kgfh51.local>
2016-04-29 12:42:16 -0500

    almost finished

--- Road Kill Facts/informationPage.swift
+++ Road Kill Facts/informationPage.swift
@@ -11,5 +11,24 @@ import UIKit
 class informationPage: UIViewController
 {
     @IBOutlet weak var outputText: UITextView!
+    
+    override func viewDidLoad()
+    {
+        var stuff0 = [String]()
+        stuff0.append("blah blah blah")
+        stuff0.append("blah blah")
+        var stuff1 = [String]()
+        stuff1.append("blah blah blah")
+        stuff1.append("blah blah")
+        var stuff2 = [String]()
+        stuff2.append("blah blah blah")
+        stuff2.append("blah blah")
+        outputText.text = ""+stuff0[rand(stuff0.count)]+"\n"+stuff1[rand(stuff1.count)]+"\n"+stuff2[rand(stuff2.count)];
+    }
+    
+    func rand(i: Int) ->Int
+    {
+        return Int(arc4random_uniform(UInt32(i)))
 
+    }
 }
--- Road Kill Facts/loadingPage.swift
+++ Road Kill Facts/loadingPage.swift
@@ -10,5 +10,25 @@ import UIKit
 
 class loadingPage: UIViewController
 {
-
+    @IBOutlet var imageView: UIImageView!
+    var int = 0;
+    var timer = NSTimer()
+    
+    override func viewDidLoad()
+    {
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.5, target: self, selector: "update", userInfo: nil, repeats: true)
+        var nextTimer =  NSTimer.scheduledTimerWithTimeInterval(5, target: self, selector: "next", userInfo: nil, repeats: false)
+    }
+    
+    func update()
+    {
+        imageView.image = UIImage(named: String(int))
+        int++
+        if(int>10){timer.invalidate()}
+    }
+    
+    func next()
+    {
+        performSegueWithIdentifier("show", sender: self)
+    }
 }
